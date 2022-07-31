efb7fd1d67a9f61fc48b4a97a495cfdca3fe9477
Gabriel de Souza Martins <83505259+gabrielglp@users.noreply.github.com>
2022-07-30 23:20:18 -0300

    Upgrade

--- components/app.js
+++ components/app.js
@@ -1,35 +1,56 @@
 const submit = document.querySelector('.submit')
-const card_1 = document.querySelector('.card-1')
-const card_2 = document.querySelector('.card-2')
-const btn_div = document.querySelectorAll('.btn-div')
-const score = document.querySelector('.score')
-let starScore = 3
+const card1Form = document.querySelector('.Card-1 form')
+const card1 = document.querySelector('.Card-1')
+const card1Btn = document.querySelectorAll('.Card-1 .CardRate input')
+const card2 = document.querySelector('.Card-2')
+const score = document.querySelector('.Card-2 span.score')
 
-submit.addEventListener('click', onSubmit)
-btn_div.forEach((btn) => {
-  btn.addEventListener('click', handleRatingBtnClick)
+// coloquei os submit no valor booleanas
+function On_Submit() {
+  submit.disabled = false
+}
+
+function OffSubmit() {
+  submit.disabled = true
+}
+
+// passando um loop para percorrer o array - e adicionando um evento change
+card1Btn.forEach((btnOptions) => {
+  btnOptions.addEventListener('change', (e) => HandleRating(e))
 })
 
-function onSubmit() {
-  card_1.classList.add('ocult')
-  score.textContent = starScore
-  card_2.classList.remove('ocult')
-}
+/* passei uma função com evento
+   uma constante passando o evento com o target 
+   assim fazendo referencia ao meu objeto */
+function HandleRating(e) {
+  const btnOptions = e.target
 
-function handleRatingBtnClick(e) {
-  btn_div.forEach((btn) => {
-    btn.classList.remove('active')
-  })
-
-  // mas funciona se clicar na parte do número do botão
-  if (e.target.classList.contains('btn-div')) {
-    e.target.classList.add('active')
-  } else {
-    e.target.parentElement.classList.add('active')
+  // e passando o controle de estado do submit
+  if (!btnOptions.checked) {
+    OffSubmit()
+    return
   }
 
-  // pontuação de estrelas parse
-  starScore = e.target.textContent
+  On_Submit()
 }
 
-console.log(btn_div)
+// passando um evento submit (ele é um dos metodos dentro de formulario *form*)
+card1Form.addEventListener('submit', (e) => OnSubmit(e))
+
+// usando o preventDefault para previnir o comportamento padrão do evento
+function OnSubmit(e) {
+  e.preventDefault()
+
+  // o FormData tem o papel de construir um conjunto de valores do card1Form
+  const formData = new FormData(card1Form)
+  // retorna o primeiro valor da chave fornecida do objeto
+  const value = formData.get('BtnInput')
+
+  // caso o objeto seja diferente de vazio, execute as condições
+  if (value != '') {
+    console.log(value)
+    score.innerText = `You selected ${value} out of 5`
+    card1.classList.add('ocult')
+    card2.classList.remove('ocult')
+  }
+}
